e2fabc86006ad43612468307856bf0ebf9fadd04
Karale <vishant.karale@capgemini.com>
2018-11-29 16:05:28 +0530

    Add new files

--- demo/java/com/example/demo/rest/RestController.java
+++ demo/java/com/example/demo/rest/RestController.java
@@ -1,10 +1,11 @@
 package com.example.demo.rest;
 
 public class RestController {
-
-	public static void main(String[] args) {
-		
-		System.out.println("In Rest Controller");
+	
+	
+	
+	public static void main() {
+		System.out.println("In rest controller");
 	}
 
 }
